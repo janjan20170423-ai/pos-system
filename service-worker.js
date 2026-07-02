40f37f69752a8aa195db3b8b83e48a85230e7809
janjan20170423-ai <janjan20170423@gmail.com>
2026-07-03 03:33:10 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+﻿const CACHE_NAME = "order-app-cache-v1";
+const urlsToCache = [
+  "/pos-system/",
+  "/pos-system/index.html",
+  "/pos-system/manifest.json",
+  "/pos-system/icon.png"
+];
+
+
+// 安裝時快取檔案
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// 攔截請求，優先回傳快取
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
